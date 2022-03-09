1057ce853837694b8488310f3e997225ca527f89
SagiriChan <nairit.sahu18@gmail.com>
2022-03-09 22:11:56 +0200

    02

--- Nuget Tehtävä/Program.cs
+++ Nuget Tehtävä/Program.cs
@@ -6,25 +6,21 @@ namespace Nuget_Tehtävä
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("How many people?");
+            
+            int personCount = Convert.ToInt32(Console.ReadLine());
+
             var name = Faker.Name.FullName();
-            var name2 = Faker.Name.FullName();
-            var name3 = Faker.Name.FullName();
-
             var address = Faker.Address.StreetAddress();
-            var address2 = Faker.Address.StreetAddress();
-            var address3 = Faker.Address.StreetAddress();
-
             var usPassport = Faker.Identification.UsPassportNumber();
-            var usPassport2 = Faker.Identification.UsPassportNumber();
-            var usPassport3 = Faker.Identification.UsPassportNumber();
-
             var ssn = Faker.Identification.SocialSecurityNumber();
-            var ssn2 = Faker.Identification.SocialSecurityNumber();
-            var ssn3 = Faker.Identification.SocialSecurityNumber();
 
-            Console.WriteLine("Name: " + name + "\n" + "Address: " + address + "\n" + "Passport number: " + usPassport + "\n" + "Social security number: " + ssn + "\n");
-            Console.WriteLine("Name: " + name2 + "\n" + "Address: " + address2 + "\n" + "Passport number: " + usPassport2 + "\n" + "Social security number: " + ssn2 + "\n");
-            Console.WriteLine("Name: " + name3 + "\n" + "Address: " + address3 + "\n" + "Passport number: " + usPassport3 + "\n" + "Social security number: " + ssn3 + "\n");
+            for (int i = 0; i < personCount; i++)
+            {
+                Console.WriteLine("Name: " + name + "\n" + "Address: " + address + "\n" + "Passport number: " + usPassport + "\n" + "Social security number: " + ssn + "\n");
+            }
         }
+
     }
 }
